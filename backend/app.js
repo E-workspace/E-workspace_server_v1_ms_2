d8f508964aa06e9aac994dcab86d390c749b23ad
E-workspace <elearn587@gmail.com>
2024-07-19 02:31:04 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
 const config = require('./config');
-
 const skillRoutes = require('./routes/skillRoutes');
 
 const app = express();
@@ -14,8 +15,16 @@ const corsOptions = {
     credentials: true,
 };
 app.use(cors(corsOptions));
-  
+app.use(cookieParser());
 app.use(bodyParser.json());
+
+const csrfProtection = csrf({ cookie: true });
+app.use(csrfProtection);
+
+// Add a route to get CSRF token
+app.get('/api/csrf-token', (req, res) => {
+    res.json({ csrfToken: req.csrfToken() });
+});
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://e-workspace-peach.vercel.app');
@@ -25,23 +34,20 @@ app.use((req, res, next) => {
     next();
 });
 
-
 // Routes
-
 app.use('/api/skills', skillRoutes);
 app.use('/api/startinterview/', skillRoutes);
-app.use('/api/SaveCode/', skillRoutes)
-app.use('/api/getSavedCode/', skillRoutes)
-app.use('/api/start-interview/', skillRoutes)
-app.use('/api/preBookedUser/', skillRoutes)
-app.use('/api/checkBooking/', skillRoutes)
-app.use('/api/Aiservice/', skillRoutes)
+app.use('/api/SaveCode/', skillRoutes);
+app.use('/api/getSavedCode/', skillRoutes);
+app.use('/api/start-interview/', skillRoutes);
+app.use('/api/preBookedUser/', skillRoutes);
+app.use('/api/checkBooking/', skillRoutes);
+app.use('/api/Aiservice/', skillRoutes);
 
 // Connect to MongoDB
 mongoose.connect(config.dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
-    
 
 const PORT = config.port || 8000;
 app.listen(PORT, () => {
